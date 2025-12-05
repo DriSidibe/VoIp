e70c9301e8255e222fe4a7ab5c69b87e0fa5748c
DriSidibe <dsidibe653@gmail.com>
2025-12-05 11:58:48 +0000

    RSA can't encrypt long messages so, use hybrid encryption with AES

--- utils/security.py
+++ utils/security.py
@@ -1,7 +1,9 @@
+import os
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 def generate_keys():
     private_key = rsa.generate_private_key(
@@ -27,25 +29,41 @@ def get_private_key(private_key):
 def encrypt_message(message: bytes, public_key: bytes) -> bytes:
     public_key = serialization.load_pem_public_key(public_key)
     
-    ciphertext = public_key.encrypt(
-    message,
+    aes_key = os.urandom(32)
+    iv = os.urandom(16)
+
+    cipher = Cipher(algorithms.AES(aes_key), modes.CFB(iv))
+    encryptor = cipher.encryptor()
+    ciphertext = encryptor.update(message) + encryptor.finalize()
+    
+    encrypted_key = public_key.encrypt(
+    aes_key,
         padding.OAEP(
             mgf=padding.MGF1(algorithm=hashes.SHA256()),
             algorithm=hashes.SHA256(),
             label=None
         )
     )
-    return ciphertext
+    return {
+        "ciphertext": ciphertext,
+        "encrypted_key": encrypted_key,
+        "iv": iv
+    }
 
-def decrypt_message(message: bytes, private_key: bytes) -> bytes:
+def decrypt_message(encrypted_block: dict, private_key: bytes) -> dict:
     private_key = serialization.load_pem_private_key(private_key, password=None)
 
-    plaintext = private_key.decrypt(
-    message,
+    aes_key = private_key.decrypt(
+    encrypted_block['encrypted_key'],
         padding.OAEP(
             mgf=padding.MGF1(algorithm=hashes.SHA256()),
             algorithm=hashes.SHA256(),
             label=None
         )
     )
+    
+    cipher = Cipher(algorithms.AES(aes_key), modes.CFB(encrypted_block["iv"]))
+    decryptor = cipher.decryptor()
+    plaintext = decryptor.update(encrypted_block["ciphertext"]) + decryptor.finalize()
+    
     return plaintext
